272a4cca483d41c1f5d2c67e56d15f586c06a845
Wiam_Laarabi <wiamlaarabi2001@gmail.com>
2023-11-06 19:50:41 +0100

    Écriture d'un bean CDI GestionnaireCompte

---
+++ src/main/java/fr/laarabi/tpbanquelaarabi/service/GestionnaireCompte.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package fr.laarabi.tpbanquelaarabi.service;
+
+import fr.laarabi.tpbanquelaarabi.entities.CompteBancaire;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root",
+        password = "root",
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@RequestScoped
+public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    @Transactional
+    public void creerCompte(CompteBancaire compteBancaire) {
+        em.persist(compteBancaire);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        Query query = em.createNamedQuery("Banque.findAll");
+        return query.getResultList();
+    }
+
+}
